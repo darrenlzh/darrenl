c37189d708feab57b4be18dc43445911f617e198
Darren Lim <darrenlzh@gmail.com>
2019-03-22 02:01:44 -0400

    updates to main scss and re-add font awesome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app/app';
-import './main.scss';
+import './scss/main.scss';
 
 render(<App />, document.getElementById('app'));
--- webpack.common.js
+++ webpack.common.js
@@ -2,33 +2,33 @@ const path = require("path");
 let HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    main: "./src/index.js",
-    vendor: "./src/vendor.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: ["html-loader"]
-      },
-      {
-        test: /\.(svg|png|jpg|gif)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name].[hash].[ext]",
-            outputPath: "assets"
-          }
-        }
-      },
+	entry: {
+		main: "./src/index.js",
+		vendor: "./src/vendor.js"
+	},
+	module: {
+		rules: [
 			{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+				test: /\.html$/,
+				use: ["html-loader"]
+			},
+			{
+				test: /\.(svg|png|jpg|gif)$/,
+				use: {
+					loader: "file-loader",
+					options: {
+						name: "[name].[hash].[ext]",
+						outputPath: "assets"
+					}
+				}
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	}
 };
